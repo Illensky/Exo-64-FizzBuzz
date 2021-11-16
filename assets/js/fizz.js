4840eea77ee667544c8ddeef34bc3eb3365b08b8
Illensky <alexis.laroche.02240@gmail.com>
2021-11-16 09:21:33 +0100

    switch from prompt/alert to while loop from 0 to 100

--- assets/js/fizz.js
+++ assets/js/fizz.js
@@ -1,20 +1,14 @@
-let number = prompt('azy aboul un nombre oklm !!!')
+let a = 1
 
-let mutiple3 = (number%3)===0 && (number%5)!==0;
-let multiple5 = (number%5)===0 && (number%3)!==0;
-let multiple53 = (number%3)===0 && (number%5)===0;
-
-if(mutiple3){
-    alert('fizz')
-}
-
-else if(multiple5){
-    alert('buzz')
-}
-
-else if (multiple53) {
-    alert('fizzbuzz')
-}
-else {
-    alert(number)
+while (a <= 100) {
+    if (((a % 3) === 0) && ((a % 5) !== 0)) {
+        console.log('fizz')
+    } else if (((a % 5) === 0) && ((a % 3) !== 0)) {
+        console.log('buzz')
+    } else if (((a % 3) === 0) && ((a % 5) === 0)) {
+        console.log('fizzbuzz')
+    } else {
+        console.log(a)
+    }
+    a++
 }
